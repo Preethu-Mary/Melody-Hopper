14d26f53de6b551831bfa7ea0fca02a661a916aa
Preethu Mary George <preethuedin@gmail.com>
2024-10-20 19:57:15 -0400

    separate helper functions to utils file

--- melody_hopper/src/Components/MicButton/MicButton.jsx
+++ melody_hopper/src/Components/MicButton/MicButton.jsx
@@ -1,5 +1,6 @@
 import './MicButton.scss';
 import { useRef, useState, useEffect } from 'react';
+import { autoCorrelate } from '../../utils/pitchtrack.js';
 
 const MicButton = () => {
     const mic = useRef(null);
@@ -15,51 +16,6 @@ const MicButton = () => {
     const source = useRef(null);
     const buffer = useRef(new Float32Array(1024)); 
     const rafID = useRef(null); 
-
-    const autoCorrelate = (buf, sampleRate) => {
-        const SIZE = buf.length;
-        let rms = 0;
-
-        for (let i = 0; i < SIZE; i++) {
-            const val = buf[i];
-            rms += val * val;
-        }
-        rms = Math.sqrt(rms / SIZE);
-        if (rms < 0.01) return -1; 
-
-        let r1 = 0, r2 = SIZE - 1, thres = 0.2;
-        for (let i = 0; i < SIZE / 2; i++)
-            if (Math.abs(buf[i]) < thres) { r1 = i; break; }
-        for (let i = 1; i < SIZE / 2; i++)
-            if (Math.abs(buf[SIZE - i]) < thres) { r2 = SIZE - i; break; }
-
-        buf = buf.slice(r1, r2);
-        const newSize = buf.length;
-
-        const c = new Array(newSize).fill(0);
-        for (let i = 0; i < newSize; i++)
-            for (let j = 0; j < newSize - i; j++)
-                c[i] += buf[j] * buf[j + i];
-
-        let d = 0; 
-        while (c[d] > c[d + 1]) d++;
-        let maxval = -1, maxpos = -1;
-
-        for (let i = d; i < newSize; i++) {
-            if (c[i] > maxval) {
-                maxval = c[i];
-                maxpos = i;
-            }
-        }
-        let T0 = maxpos;
-
-        const x1 = c[T0 - 1], x2 = c[T0], x3 = c[T0 + 1];
-        const a = (x1 + x3 - 2 * x2) / 2; 
-        const b = (x3 - x1) / 2; 
-        if (a) T0 = T0 - b / (2 * a);
-
-        return sampleRate / T0;
-    };
 
     const getPitch = () => {
         if (!analyser.current) return; 
--- melody_hopper/src/utils/pitchtrack.js
+++ melody_hopper/src/utils/pitchtrack.js
@@ -1,152 +1,44 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const mic = document.getElementById("mic");
-//     const constraints = {
-//         audio: true,
-//         video: false 
-//     }
+export const autoCorrelate = (buf, sampleRate) => {
+    const SIZE = buf.length;
+    let rms = 0;
 
-//     // const note = document.getElementById("note");
-//     // const hz = document.getElementById("hz");
-//     // const detune = document.getElementById("detune");
-//     // const noteString = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+    for (let i = 0; i < SIZE; i++) {
+        const val = buf[i];
+        rms += val * val;
+    }
+    rms = Math.sqrt(rms / SIZE);
+    if (rms < 0.01) return -1; 
 
-//     let currentStream = null;
-//     // eslint-disable-next-line no-unused-vars
-//     let source = null;
-//     let audioContext = null; 
-//     // let analyser = null;
-//     // let buffer = new Float32Array(1024);
-//     let rafID = null;
+    let r1 = 0, r2 = SIZE - 1, thres = 0.2;
+    for (let i = 0; i < SIZE / 2; i++)
+        if (Math.abs(buf[i]) < thres) { r1 = i; break; }
+    for (let i = 1; i < SIZE / 2; i++)
+        if (Math.abs(buf[SIZE - i]) < thres) { r2 = SIZE - i; break; }
 
-//     // function noteFromPitch( frequency ) {
-//     //     var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
-//     //     return Math.round( noteNum ) + 69;
-//     // }
+    buf = buf.slice(r1, r2);
+    const newSize = buf.length;
 
-//     // function frequencyFromNoteNumber( note ) {
-//     //     return 440 * Math.pow(2,(note-69)/12);
-//     // }
-    
-//     // function centsOffFromPitch( frequency, note ) {
-//     //     return Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
-//     // }
+    const c = new Array(newSize).fill(0);
+    for (let i = 0; i < newSize; i++)
+        for (let j = 0; j < newSize - i; j++)
+            c[i] += buf[j] * buf[j + i];
 
-//     function autoCorrelate( buf, sampleRate ) {
-//         // Implements the ACF2+ algorithm
-//         var SIZE = buf.length;
-//         var rms = 0;
-    
-//         for (var i=0;i<SIZE;i++) {
-//             var val = buf[i];
-//             rms += val*val;
-//         }
-//         rms = Math.sqrt(rms/SIZE);
-//         if (rms<0.01) // not enough signal
-//             return -1;
-    
-//         var r1=0, r2=SIZE-1, thres=0.2;
-//         for (var i=0; i<SIZE/2; i++)
-//             if (Math.abs(buf[i])<thres) { r1=i; break; }
-//         for (var i=1; i<SIZE/2; i++)
-//             if (Math.abs(buf[SIZE-i])<thres) { r2=SIZE-i; break; }
-    
-//         buf = buf.slice(r1,r2);
-//         SIZE = buf.length;
-    
-//         var c = new Array(SIZE).fill(0);
-//         for (var i=0; i<SIZE; i++)
-//             for (var j=0; j<SIZE-i; j++)
-//                 c[i] = c[i] + buf[j]*buf[j+i];
-    
-//         var d=0; while (c[d]>c[d+1]) d++;
-//         var maxval=-1, maxpos=-1;
-//         for (var i=d; i<SIZE; i++) {
-//             if (c[i] > maxval) {
-//                 maxval = c[i];
-//                 maxpos = i;
-//             }
-//         }
-//         var T0 = maxpos;
-    
-//         var x1=c[T0-1], x2=c[T0], x3=c[T0+1];
-//         a = (x1 + x3 - 2*x2)/2;
-//         b = (x3 - x1)/2;
-//         if (a) T0 = T0 - b/(2*a);
-    
-//         return sampleRate/T0;
-//     }
+    let d = 0; 
+    while (c[d] > c[d + 1]) d++;
+    let maxval = -1, maxpos = -1;
 
-//     function startPitchTrack() {
-//        analyser = audioContext.createAnalyser();
-//        analyser.fftSize = 2048;
-//        source.connect(analyser);
+    for (let i = d; i < newSize; i++) {
+        if (c[i] > maxval) {
+            maxval = c[i];
+            maxpos = i;
+        }
+    }
+    let T0 = maxpos;
 
-//         getPitch();
-//     }
+    const x1 = c[T0 - 1], x2 = c[T0], x3 = c[T0 + 1];
+    const a = (x1 + x3 - 2 * x2) / 2; 
+    const b = (x3 - x1) / 2; 
+    if (a) T0 = T0 - b / (2 * a);
 
-//     // function getPitch() {
-//     //    analyser.getFloatTimeDomainData(buffer);
-//     //    let frequencyInHz = autoCorrelate(buffer, audioContext.sampleRate);
-//     //    console.log(frequencyInHz);
-
-//     //    rafID = window.requestAnimationFrame(getPitch);
-
-//     //    let midiNote = noteFromPitch(frequencyInHz)
-//     //    note.innerHTML = noteString[midiNote % 12];
-
-//     //    let off = centsOffFromPitch(frequencyInHz, midiNote);
-
-//     //    if (off <30 && off > -30) {
-//     //     detune.innerHTML = "In tune";
-//     //    }
-//     //    else {
-//     //     detune.innerHTML = "not In tune";
-//     //    }
-//     // }
-
-//     function getMicrophoneStream() {
-//         navigator.mediaDevices
-//         .getUserMedia(constraints)
-//         .then((stream) => {
-//             currentStream = stream;
-//             console.log(stream);
-
-//             audioContext = new AudioContext();
-//             source = audioContext.createMediaStreamSource(stream);
-
-//             // startPitchTrack();
-// ;        })
-//         .catch(() => {
-//             alert("Failed to turn the microphone on!");
-//             mic.innerHTML = "Enable mic";
-//         });
-//     }
-
-//     function stopMicrophoneStream() {
-//         if (currentStream !== null) {
-//             let tracks = currentStream.getTracks();
-//             for(let i=0; i<tracks.length; i++) {
-//                 tracks[i].stop();
-//             }
-//         }
-
-//         window.cancelAnimationFrame(rafID);
-//     }
-
-//     function main() {
-//         let isTracking = mic.getAttribute("data-tracking") === "true";
-//         mic.setAttribute("data-tracking", !isTracking);
-
-//         if (!isTracking) {
-//             mic.innerHTML = "Enabling...";
-
-//             getMicrophoneStream();
-//             mic.innerHTML = "Disable mic";
-//         } else {
-//             mic.innerHTML = "Enable mic";
-//             stopMicrophoneStream();
-//         }
-//     }
-
-//     mic.onclick = main;
-// });
+    return sampleRate / T0;
+};
